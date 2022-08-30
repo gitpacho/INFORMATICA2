9f497f23931a4ba1278b1ec182680cf84f382395
gitpacho <crepachonpa@unal.edu.co>
2022-08-30 14:51:26 -0500

    operadores logicos

--- EJERCICIOS/2_Operadores.py
+++ EJERCICIOS/2_Operadores.py
@@ -1,1 +1,45 @@
-#
+# Operador de asignación
+
+a = 1
+b = 3
+c = 5
+
+# Operadores aritméticos
+""" realice las siguientes operaciones mentalmente:   3 + 9
+                                                      3.0 + 9
+                                                      9 ** 0.5
+                                                      2 ** 32
+                                                      19 // 2 
+                                                      19 % 3
+                                                      9/3
+                                                      "hola" + "mundo"
+                                                      "hola" * 3
+                                                      ["A"] + [1,2,3]
+                                                      [] + []
+                                                      (1,2,3) + (1,)  
+                                                      """
+
+
+# Operadores lógicos
+""" realice las siguientes operaciones mentalmente:  True and True
+                                                     False and True
+                                                     False and False
+                                                     not True
+                                                     not False
+                                                     True or True
+                                                     False or True
+                                                     1 and 1
+                                                     0 and 1
+                                                     1 and 3
+                                                     1 and "hola"
+                                                     0 and 3
+                                                     0 and "hola"
+                                                     1 or 3
+                                                     "hola" or "verdadero"
+                                                     "" or "hola"
+                                                     "" or False
+
+
+ """
+
+print("" or False)

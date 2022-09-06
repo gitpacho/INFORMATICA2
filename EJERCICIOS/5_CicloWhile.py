c862416788724c98a59fea05cfb4daff15e0215c
gitpacho <crepachonpa@unal.edu.co>
2022-09-06 15:56:55 -0500

    ejercicios pendientes while

--- EJERCICIOS/5_CicloWhile.py
+++ EJERCICIOS/5_CicloWhile.py
@@ -22,3 +22,19 @@ un ciclo, el programa pide adivinar el numero, en caso de no ser acertado
 se debe mostrar un mensaje diciendo: "Estás atrapado". Y en caso contrario
 terminar el ciclo y avisar que el numero es correcto.
 """
+#numero_secreto = 999
+#numero_usuario = int(input("Ingrese el número secreto: "))
+#condicion = (numero_secreto != numero_usuario)
+#
+#while condicion:
+#    print("No es el número correcto")
+
+"""
+Realice un ciclo while que imprima los números del 10 al 100, separados por guion(-)
+sin salto de linea
+10 - 11 - 12 - 13 - ... 100
+"""
+
+
+
+

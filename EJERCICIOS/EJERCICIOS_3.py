8f5f737822177bc34c0851f51a625b92302ba8d7
gitpacho <crepachonpa@unal.edu.co>
2022-09-13 16:57:57 -0500

    Actualización contenido

--- EJERCICIOS/EJERCICIOS_3.py
+++ EJERCICIOS/EJERCICIOS_3.py
@@ -38,7 +38,7 @@
 """ El rendimiento de los empleados de una empresa es el siguiente:
 
 --------------  Emplea_1  Emplea_2  Emplea_3  Emplea_4  Emplea_5  Emplea_6  Emplea_7  Emplea_8  Emplea_9  Emplea_10  Emplea_11  Emplea_12  Emplea_13  Emplea_14  Emplea_15  Emplea_16  Emplea_17  Emplea_18  Emplea_19  Emplea_20  Emplea_21  Emplea_22  Emplea_23  Emplea_24  Emplea_25  Emplea_26  Emplea_27 
-Rendimiento --    "C"       "B"      "B"        "B"        "C"      "B"      "A"        "C"       "B"        "A"        "C"       "B"        "B"        "B"         "B"        "A"       "B"        "A"         "A"        "C"       "B"        "B"        "B"         "B"         "C"       "A" 
+Rendimiento --    "C"       "B"      "B"        "B"        "C"      "B"      "A"        "C"       "B"        "A"        "C"       "B"        "B"        "B"         "B"        "A"       "B"        "A"         "A"        "C"       "B"        "B"        "B"         "B"         "C"       "A"       "C"   
 
 Donde "A" es alto, "B" aceptable  y "C"  insuficiente. 
 Determine ¿cuales empleados pueden solicitar un aumento salarial y cuáles pueden ser despedidos? """

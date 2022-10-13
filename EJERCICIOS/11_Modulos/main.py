29042b110d3ed143c3244efe3dbd0d4aa6ed5a54
gitpacho <crepachonpa@unal.edu.co>
2022-10-13 15:49:15 -0500

    changes

--- EJERCICIOS/11_Modulos/main.py
+++ EJERCICIOS/11_Modulos/main.py
@@ -12,4 +12,13 @@ respuesta = [1,2,3]
 interfaz.imprimirVariable("Respuesta", respuesta)
 
 
+import logica
+interfaz.separador("--")
+logica.sumar3Numeros(1,2,3)
+logica.sumarNumeros(1,2,3,4,5,6,7,8,9)
+logica.sumarListas([1,2,3], [3,2,1])
+
+
+
+
 

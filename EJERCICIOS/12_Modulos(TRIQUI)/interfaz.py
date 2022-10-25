bfbc2159e42d9b33c5c17e8e143a691150fe1b79
gitpacho <crepachonpa@unal.edu.co>
2022-10-25 15:44:28 -0500

    triqui

---
+++ EJERCICIOS/12_Modulos(TRIQUI)/interfaz.py
@@ -0,0 +1,24 @@
+"""
+Este modulo contiene 3 funciones para imprimir en el juego triqui
+"""
+
+def explicarJuego():
+    explicacion = """
+    ====================================================
+    Este es el juego triqui
+    
+    Es un juego de 2 jugadores, para ganar debe 
+    completar 3 caracteres iguales en linea ("x" o "o")
+    
+    Para ingresar la posicion en el juego, guiese
+    con los siguientes numeros
+
+                0  | 1 |  2
+                ------------- 
+                3  | 4 |  5
+                ------------- 
+                6  | 7 |  8
+
+    =====================================================
+    """
+    print(explicacion)

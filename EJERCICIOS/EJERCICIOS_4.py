f5503e4cabc25d3fae29a237136d28297723c698
gitpacho <crepachonpa@unal.edu.co>
2022-10-27 15:05:07 -0500

    Ejercicios_4

--- EJERCICIOS/EJERCICIOS_4.py
+++ EJERCICIOS/EJERCICIOS_4.py
@@ -9,9 +9,9 @@
 """
 Cree los modulos que indica cada ejercicio, para desarrollar los siguientes programas:
 
-1) Algoritmo para jugar un sudoku 4*4 => modulos: logica, interfaz, control
-2) Herramienta de calificaciones      => modulos: estudiante, materia, calculos, control
-3) Algoritmo conversor de numeros     => modulos: decimal binario hexadecimal octal, control
+1) Algoritmo conversor de numeros     => modulos: decimal binario hexadecimal octal, control
+2) Algoritmo para jugar un sudoku 4*4 => modulos: logica, interfaz, control
+3) Herramienta de calificaciones      => modulos: estudiante, materia, calculos, control
 4) Algoritmo operador de vectores     => modulos: vector, operacionesVectores, control
 5) Algoritmo operador de matrices     => modulos: matriz, operacionesMatrices, control 
 

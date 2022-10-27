abed9f1639971fec1b598428d166f001f48b4075
gitpacho <crepachonpa@unal.edu.co>
2022-10-27 14:54:45 -0500

    updates

--- EJERCICIOS/12_Modulos_TRIQUI/control.py
+++ EJERCICIOS/12_Modulos_TRIQUI/control.py
@@ -5,8 +5,11 @@ interfaz.explicarJuego()
 tableroJuego = logica.obtenerTableroLogico()
 
 for turno in ["x", "o", "x", "o","x", "o","x", "o", "x"]:
-    print("Turno jugador ", turno)
+    print("\nTurno jugador ", turno)
     posicion = int(input("   Ingrese posicion de juego: "))
     tableroJuego = logica.actualizarTableroLogico(tableroJuego, posicion, turno)
     interfaz.dibujarTablero(tableroJuego)
     posibleGanador = logica.determinarGanador(tableroJuego)
+    if posibleGanador in ["x", "o"]:
+        print("Felicidades ha ganado el juego jugador ", turno)
+        break

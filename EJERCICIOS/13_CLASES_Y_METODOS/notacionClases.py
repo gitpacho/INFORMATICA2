194610fcb7e0200c110c4352fc1659ee9be0cf5a
gitpacho <crepachonpa@unal.edu.co>
2022-11-08 14:58:34 -0500

    comida colombiana updates

--- EJERCICIOS/13_CLASES_Y_METODOS/notacionClases.py
+++ EJERCICIOS/13_CLASES_Y_METODOS/notacionClases.py
@@ -7,11 +7,12 @@ metodos: nutrir y provocar
 """
 
 class ComidaColombiana:
+    #Para crear los atributos
     def __init__(self, ingrediente1,ingrediente2, ingrediente3):
         self.ingrediente1 = ingrediente1
         self.ingrediente2 = ingrediente2
         self.ingrediente3 = ingrediente3
     
+    
 #Como creo el objeto
-
 bandejaPaisa = ComidaColombiana()

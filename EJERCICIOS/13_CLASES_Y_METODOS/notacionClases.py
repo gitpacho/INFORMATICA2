225c6ce60018045ac69afc92914e22c432ec1930
gitpacho <crepachonpa@unal.edu.co>
2022-11-08 15:24:31 -0500

    metodos

--- EJERCICIOS/13_CLASES_Y_METODOS/notacionClases.py
+++ EJERCICIOS/13_CLASES_Y_METODOS/notacionClases.py
@@ -13,6 +13,34 @@ class ComidaColombiana:
         self.ingrediente2 = ingrediente2
         self.ingrediente3 = ingrediente3
     
-    
-#Como creo el objeto
-bandejaPaisa = ComidaColombiana()
+    #Para crear metodos (funciones)
+    def provocar(self, opcion):
+        if opcion in ["huele bien", "se ve bien", "tengo hambre"]:
+            return "Este alimento provoca"
+        else:
+            return "Este alimento no provoca"
+
+    def nutrir(self, opcion):
+        if opcion in ["estoy enfermo", "tengo nauseas", "el doctor me prohibio"]:
+            return "Este almiento no me puede nutrir"
+        else:
+            return "Este alimento me nutre"
+
+
+#Como creo el objeto ?
+bandejaPaisa = ComidaColombiana("chorizo", "arepa", "chicharron")
+sancochoDeGallina = ComidaColombiana("gallina", "papa", "caldo")
+ajiacoSantafereño = ComidaColombiana("papa", "pollo", "aguacate")
+print("Es instancia? =>", isinstance(ajiacoSantafereño, ComidaColombiana))
+
+#Cómo accedo a los atributos de un objeto
+atributoA = bandejaPaisa.ingrediente3
+atributoB = sancochoDeGallina.ingrediente1
+atributoC = ajiacoSantafereño.ingrediente2
+print("atributos =>", atributoA, atributoB, atributoC)
+
+
+#Cómo acceder a los métodos de un objeto
+
+
+print(ComidaColombiana.__dict__)

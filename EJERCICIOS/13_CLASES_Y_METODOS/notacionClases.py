39851daa95df38e252d87a5ff03b502978a6d368
gitpacho <crepachonpa@unal.edu.co>
2022-11-08 14:54:28 -0500

    ejemplo comida colombiana

---
+++ EJERCICIOS/13_CLASES_Y_METODOS/notacionClases.py
@@ -0,0 +1,17 @@
+# 08-11-2022
+
+"""
+Crear una clase llamada ComidaColombiana
+atributos: ingrediente1, ingrediente2, ingrediente3
+metodos: nutrir y provocar
+"""
+
+class ComidaColombiana:
+    def __init__(self, ingrediente1,ingrediente2, ingrediente3):
+        self.ingrediente1 = ingrediente1
+        self.ingrediente2 = ingrediente2
+        self.ingrediente3 = ingrediente3
+    
+#Como creo el objeto
+
+bandejaPaisa = ComidaColombiana()

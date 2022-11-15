e4324864bc978d7297d6ff6067e9d5d8489fc1b8
gitpacho <crepachonpa@unal.edu.co>
2022-11-15 15:26:49 -0500

    lectura json

--- EJERCICIOS/14_PersistenciaDatos/persistencia_lectura.py
+++ EJERCICIOS/14_PersistenciaDatos/persistencia_lectura.py
@@ -23,6 +23,21 @@ Leer el archivo estudiantes.json y luego calcular el promedio
 de cada uno de los estudiantes
 """
 
+archivo = "estudiantes.json"
+ruta = "EJERCICIOS/14_PersistenciaDatos/" + archivo
+opcion = "r"
+
+with open(ruta, opcion) as file:
+    data = json.load(file)
+
+for nombre in data.keys():
+    print(nombre, " => ", sum(data[nombre])/3)
+
+
+
+
+
+
 
 
 

dd519f57d853bc5ca7621277c721d44e7863e43b
gitpacho <crepachonpa@unal.edu.co>
2022-11-15 15:48:37 -0500

    clases Estudiante-Curso

---
+++ EJERCICIOS/14_PersistenciaDatos/estudiantes_cursos.py
@@ -0,0 +1,46 @@
+class Estudiante:
+    def __init__(self, nombre, edad, codigo, notas):
+        self.nombre = nombre
+        self.edad = edad
+        self.codigo = codigo
+        self.notas = notas
+
+    def calcularPromedio(self):
+        pass
+
+    def determinarMejorNota(self):
+        pass
+
+    def determinarPeorNota(self):
+        pass
+
+
+class Curso:
+    def __init__(self, nombreCurso, nombreProfesor, estudiantes):
+        self.nombreCurso = nombreCurso
+        self.nombreProfesor = nombreProfesor
+        self.estudiantes = estudiantes
+
+    def calcularMediaDelCurso(self):
+        pass
+
+    def determinarEstudiantesAprobados(self):
+        pass
+
+    def determinarEstudiantesReprobados(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

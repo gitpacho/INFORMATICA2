568bc73568237ec58e73b9e4c937d4d2689db510
gitpacho <crepachonpa@unal.edu.co>
2022-11-15 15:23:20 -0500

    lectura json

---
+++ EJERCICIOS/14_PersistenciaDatos/persistencia_lectura.py
@@ -0,0 +1,28 @@
+#15-11-2022
+
+#---------- Lectura de archivos json
+
+
+"""Leer el archivo DATA.json
+y luego calcular la media de los valores
+"""
+
+import json
+archivo = "DATA.json"
+ruta = "EJERCICIOS/14_PersistenciaDatos/" + archivo
+opcion = "r" #lectura
+
+with open(ruta, opcion) as file:
+    data = json.load(file)
+
+print("media=>", sum(data)/len(data))
+
+
+"""
+Leer el archivo estudiantes.json y luego calcular el promedio
+de cada uno de los estudiantes
+"""
+
+
+
+

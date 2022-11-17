078b0a794b5f5f1ef2e6b7a9afdef59c076063ea
gitpacho <crepachonpa@unal.edu.co>
2022-11-17 15:15:44 -0500

    updates

--- EJERCICIOS/14_PersistenciaDatos/estudiantes_cursos.py
+++ EJERCICIOS/14_PersistenciaDatos/estudiantes_cursos.py
@@ -22,38 +22,39 @@ class Curso:
     def __init__(self, nombreCurso, nombreProfesor, estudiantes):
         self.nombreCurso = nombreCurso
         self.nombreProfesor = nombreProfesor
-        self.estudiantes = estudiantes #lista de estudiantes, diccionario ....
+        self.estudiantes = estudiantes #lista con objetos de la clase Estudiantes
 
     def calcularMediaDelCurso(self):
-        media = sum(self.estudiantes.values())/len(self.estudiantes)
-        return media
+        acum = 0
+        for estudiante in self.estudiantes:
+            print(estudiante.nombre,estudiante.notas, estudiante.calcularPromedio())
+
 
     def determinarEstudiantesAprobados(self):
-        Aprobados= []
-        for nombre in self.estudiantes:
-            if self.estudiantes[nombre] >= 3.0:
-                Aprobados.append(nombre)
-        return Aprobados
+        pass
 
     def determinarEstudiantesReprobados(self):
-        Reprobados = []
-        for nombre in self.estudiantes:
-            if self.estudiantes[nombre] < 3.0:
-                Reprobados.append(nombre)
-        return Reprobados
+        pass
+        
+        
+        
+        
 
 
 
 if __name__ == "__main__":
-    estudiante1 = Estudiante("Cristian Pachon", 20, "019", [5.0, 1.0, 3.0])
-    data = {
-    "Cristian P": 1.5,
-    "Maria Jimenes": 4,
-    "Oscar Agudelo":3.5}
+    estudiante1 = Estudiante("Cristian Pachon", 20, "019", [0.0, 0.0, 1.5])
+    estudiante2 = Estudiante("Maria Jimenez", 15, "020", [5.0, 2.0, 5.0])
+    estudiante3 = Estudiante("Lesly Cañas", 19, "021", [5.0, 5.0, 5.0])
+    listaEstudiantes = [estudiante1, estudiante2, estudiante3]
 
-    print(estudiante1.calcularPromedio())
+    curso1 = Curso("Matematicas", "Cristian Pachon", listaEstudiantes)
 
-    curso1 = Curso("Matematicas", "Cristian Pachon", data)
     print("media del curso=>", curso1.calcularMediaDelCurso())
+    print("aprobados", curso1.determinarEstudiantesAprobados())
+    print("reprobados", curso1.determinarEstudiantesReprobados())
+
+
+    print(listaEstudiantes)
 
     
